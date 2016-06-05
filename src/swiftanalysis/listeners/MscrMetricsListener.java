b1ea91d6d66a5fa21101631acb81b35992b1f921
Marcel Rebouças <mscreboucas@gmail.com>
2016-06-05 16:53:46 -0300

    Adds the count of implicit/explicit declarations

--- src/swiftanalysis/listeners/MscrMetricsListener.java
+++ src/swiftanalysis/listeners/MscrMetricsListener.java
@@ -3,8 +3,12 @@ package swiftanalysis.listeners;
 import swiftanalysis.generated.SwiftBaseListener;
 import swiftanalysis.generated.SwiftParser;
 import swiftanalysis.generated.SwiftParser.ConditionClauseContext;
+import swiftanalysis.generated.SwiftParser.PatternInitializerContext;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNodeImpl;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,6 +42,11 @@ public class MscrMetricsListener extends SwiftBaseListener {
 	private static int rethrowsCounter = 0;
 	private static int catchCounter = 0;
 
+	private static int implicitVariableDeclarationsCounter = 0;
+	private static int explicitVariableDeclarationsCounter = 0;
+	private static int implicitConstantDeclarationsCounter = 0;
+	private static int explicitConstantDeclarationsCounter = 0;
+
 	private static Map<String, Integer> typeMap = new HashMap<String, Integer>();
 	private static Map<String, Integer> optionalTypeMap = new HashMap<String, Integer>();
 	private static Map<String, Integer> forcedTypeMap = new HashMap<String, Integer>();
@@ -45,13 +54,13 @@ public class MscrMetricsListener extends SwiftBaseListener {
 	// This map holds the depth of the objects receiving an optional chaining call.
 	// e.g.: window? -> 0, window.view? -> 1, window.view.layer? -> 2
 	private static Map<String, Integer> optionalChainingObjectCallDepthMap = new HashMap<String, Integer>();
-	
+
 	// This map hold the amount of optional chaining calls before the actual call.
 	// e.g.: window? -> 0, window.view? -> 0, window.view?.layer? -> 1
 	private static Map<String, Integer> optionalChainingCallDepthMap = new HashMap<String, Integer>();
-	
-	
-	
+
+
+
 	public MscrMetricsListener() {
 		//empty constructor
 	}
@@ -72,51 +81,51 @@ public class MscrMetricsListener extends SwiftBaseListener {
 
 	@Override
 	public void enterConditionClause(ConditionClauseContext ctx) {
-		
+
 		if (containsNilCheck(ctx)) {
 			equalsNilCounter++;
 		}
 	}
 
 	@Override public void enterIfStatement(SwiftParser.IfStatementContext ctx) { 
-		
+
 		ifCounter++;
 
 		ConditionClauseContext conditionClause = ctx.conditionClause();
-		
+
 		if (conditionClause.getText().startsWith("let")) {
 			ifLetCounter++;
 		}
-		
+
 		if (containsNilCheck(conditionClause)){
 			ifWithNilCheckCounter++;
 		}
 	}
 
 	@Override public void enterGuardStatement(SwiftParser.GuardStatementContext ctx) { 
-		
+
 		guardCounter++;
 
 		ConditionClauseContext conditionClause = ctx.conditionClause();
-		
+
 		if (conditionClause.getText().startsWith("let")) {
 			guardLetCounter++;
 		}
-		
+
 		if (containsNilCheck(conditionClause)){
 			guardWithNilCheckCounter++;
 		}
 	}
-	
+
 	@Override public void enterOptionalChainingExpression(SwiftParser.OptionalChainingExpressionContext ctx) {
-		
+
 		optionalChainingCounter++;
-		
+
 		String optionalChainingText = ctx.getText();
-		
+
 		int callDepth = countOccurrences(optionalChainingText,'.');
 		int chainingCallDepth = countOccurrences(optionalChainingText,'?') - 1;
-		
+
 		checkAndAdd(optionalChainingObjectCallDepthMap, Integer.toString(callDepth));
 		checkAndAdd(optionalChainingCallDepthMap, Integer.toString(chainingCallDepth));
 	}
@@ -141,7 +150,7 @@ public class MscrMetricsListener extends SwiftBaseListener {
 			} else if (secondChild.getText().equals("rethrows")) {
 				rethrowsCounter++;
 			}
-			
+
 		} else {
 			checkAndAdd(typeMap, type);
 		}
@@ -165,24 +174,68 @@ public class MscrMetricsListener extends SwiftBaseListener {
 			nilCoalescingCounter++;
 		}
 	}
-	
+
 	@Override 
 	public void enterDoStatement(SwiftParser.DoStatementContext ctx) {
 		doBlockCounter++;
 	}
-	
+
 	@Override 
 	public void enterCatchClause(SwiftParser.CatchClauseContext ctx) {
 		catchCounter++;
 	}
-	
+
 	@Override 
 	public void enterThrowStatement(SwiftParser.ThrowStatementContext ctx) {
 		throwCounter++;
 	}
-	
+
+	@Override 
+	public void enterVariableDeclaration(SwiftParser.VariableDeclarationContext ctx) {
+
+		if (ctx.patternInitializerList() != null && ctx.patternInitializerList().patternInitializer() != null){
+			for (PatternInitializerContext child : ctx.patternInitializerList().patternInitializer()) {
+				if (child.pattern().typeAnnotation() != null) {
+					if (child.pattern().typeAnnotation().getText().startsWith(":")) {
+						explicitVariableDeclarationsCounter++;
+					} else {
+						implicitVariableDeclarationsCounter++;
+					}
+				} else {
+					implicitVariableDeclarationsCounter++;
+				}
+			}
+		} else if (ctx.typeAnnotation() != null){
+			if (ctx.typeAnnotation().getText().startsWith(":")) {
+				explicitVariableDeclarationsCounter++;
+			} else {
+				implicitVariableDeclarationsCounter++;
+			}
+		} else {
+			implicitVariableDeclarationsCounter++;
+		}
+	}
+
+	@Override 
+	public void enterConstantDeclaration(SwiftParser.ConstantDeclarationContext ctx) {
+
+		if (ctx.patternInitializerList() != null && ctx.patternInitializerList().patternInitializer() != null){
+			for (PatternInitializerContext child : ctx.patternInitializerList().patternInitializer()) {
+				if (child.pattern().typeAnnotation() != null) {
+					if (child.pattern().typeAnnotation().getText().startsWith(":")) {
+						explicitConstantDeclarationsCounter++;
+					} else {
+						implicitConstantDeclarationsCounter++;
+					}
+				} else {
+					implicitConstantDeclarationsCounter++;
+				}
+			}
+		} 
+	}
+
 	private static void checkAndAdd(Map<String, Integer> map, String key){
-		
+
 		Integer value = map.get(key);
 		if (value == null) {
 			map.put(key, 1);
@@ -190,23 +243,23 @@ public class MscrMetricsListener extends SwiftBaseListener {
 			map.put(key, value+1);
 		}
 	}
-	
+
 	/**
 	 * Checks if a ConditionClause contains a nil check (==nil or != nil).
 	 * @param ctx The ConditionClauseContext to be checked.
 	 * @return True if the ConditionClause contains the nil check, False otherwise.
 	 */
 	private static boolean containsNilCheck (ConditionClauseContext ctx) {
-		
+
 		String conditionClauseText = ctx.getText();
-		
+
 		if (conditionClauseText.contains("==nil") || 
-			conditionClauseText.contains("!=nil") ||
-			conditionClauseText.contains("nil==") ||
-			conditionClauseText.contains("nil!=")) {
+				conditionClauseText.contains("!=nil") ||
+				conditionClauseText.contains("nil==") ||
+				conditionClauseText.contains("nil!=")) {
 			return true;
 		}
-		
+
 		return false;
 	}
 	/**
@@ -219,13 +272,13 @@ public class MscrMetricsListener extends SwiftBaseListener {
 	 * @return
 	 */
 	private static Map<String, Integer> removeDuplicatedValues(Map<String, Integer> map1, Map<String, Integer> map2) {
-		
+
 		Map<String, Integer> output = new HashMap<String, Integer>(map1);
-		
+
 		for (String key : map2.keySet()) {
-			
+
 			String typeName = key.substring(0, key.length()-1);
-	
+
 			if (output.containsKey(typeName)) {
 				if (map2.get(key) >= output.get(typeName)) {
 					output.remove(typeName);
@@ -235,26 +288,39 @@ public class MscrMetricsListener extends SwiftBaseListener {
 				}
 			}
 		}
-		
+
 		return output;
 	}
-	
+
 	private static int countOccurrences(String input, char c)
 	{
-	    int count = 0;
-	    for (int i=0; i < input.length(); i++)
-	    {
-	        if (input.charAt(i) == c) { count++; }
-	    }
-	    return count;
+		int count = 0;
+		for (int i=0; i < input.length(); i++)
+		{
+			if (input.charAt(i) == c) { count++; }
+		}
+		return count;
 	}
-	
+
+	/**
+	 * Returns the last token of the construct represented by node.
+	 *
+	 * @param node A node
+	 * @return Stop token
+	 */
+	public static Token getStopTokenForNode(ParseTree node) {
+		if (node instanceof TerminalNodeImpl) {
+			return ((TerminalNodeImpl) node).getSymbol();
+		} else {
+			return ((ParserRuleContext) node).getStop();
+		}
+	}
 	public static void printSummary() {
 
 		System.out.println("If: "+ ifCounter); //OK
 		System.out.println("If Let: "+ ifLetCounter); //OK
 		System.out.println("If with null check: "+ ifWithNilCheckCounter); //OK
-		
+
 		System.out.println("Guard: "+ guardCounter); //OK
 		System.out.println("Guard Let: "+ guardLetCounter); //OK
 		System.out.println("Guard with null check: "+ guardWithNilCheckCounter); //OK
@@ -262,13 +328,18 @@ public class MscrMetricsListener extends SwiftBaseListener {
 		System.out.println("Optional Chaining: "+ optionalChainingCounter); //OK
 		System.out.println("Optional Chaining Object Depth: "+ optionalChainingObjectCallDepthMap.toString()); //OK
 		System.out.println("Optional Chaining Call Depth: "+ optionalChainingCallDepthMap.toString()); //OK
-		
+
 		System.out.println("Forced Unwrapping: "+ forcedUnwrappingsCounter); //OK
 		System.out.println("== nil, != nil: "+ equalsNilCounter); //OK
 		System.out.println("??: "+ nilCoalescingCounter); //OK
 
 		System.out.println("as?: "+ optionalTypeCastingCounter); //OK
 		System.out.println("as!: "+ forcedTypeCastingCounter); //OK
+
+		System.out.println("Implicit Variable Declarations: "+ implicitVariableDeclarationsCounter); //OK
+		System.out.println("Explicit Variable Declarations: "+ explicitVariableDeclarationsCounter); //OK
+		System.out.println("Implicit Constant Declarations: "+ implicitConstantDeclarationsCounter); //OK
+		System.out.println("Explicit Constant Declarations: "+ explicitConstantDeclarationsCounter); //OK
 
 		System.out.println("Do Blocks: "+ doBlockCounter); //OK
 		System.out.println("Try: "+ tryCounter); //OK
@@ -278,14 +349,14 @@ public class MscrMetricsListener extends SwiftBaseListener {
 		System.out.println("Throws: "+ throwsCounter); //OK
 		System.out.println("Rethrows: "+ rethrowsCounter); //OK
 		System.out.println("Catch: "+ catchCounter); //OK
-		
+
 		Map<String, Integer> fixedMap = removeDuplicatedValues(removeDuplicatedValues(typeMap, optionalTypeMap), forcedTypeMap);
-		
+
 		System.out.println("Normal types : "+ fixedMap.size());
 		System.out.println("Optional Types: "+ optionalTypeMap.size()); //OK
 		System.out.println("Implicit Unwrapped Types: "+ forcedTypeMap.size()); //OK
 
 	}
-	
-	
+
+
 }
